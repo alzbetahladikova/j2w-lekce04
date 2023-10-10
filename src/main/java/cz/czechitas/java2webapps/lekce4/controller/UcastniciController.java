c77a8cae81a77651ab016876dbf3b9aecc0cd513
alzbetahladikova <beta.strelbova@gmail.com>
2023-10-10 20:21:36 +0200

    základní provedení hotovo

--- src/main/java/cz/czechitas/java2webapps/lekce4/controller/UcastniciController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce4/controller/UcastniciController.java
@@ -1,16 +1,36 @@
 package cz.czechitas.java2webapps.lekce4.controller;
 
+import cz.czechitas.java2webapps.lekce4.entity.Lekce;
+import cz.czechitas.java2webapps.lekce4.entity.Ucastnici;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller()
 @RequestMapping("/ucastnici")
 public class UcastniciController {
+
+  private final List<Ucastnici> seznamUcastniku = List.of(
+
+          new Ucastnici ("Ája","Fíková", "Ahníkov"),
+          new Ucastnici("Maxipes", "FÍk","Ahníkov"),
+          new Ucastnici("Zajíc", "Odlesa","U Rybníka")
+  );
   @GetMapping("/")
   public ModelAndView seznam() {
     ModelAndView modelAndView = new ModelAndView("/ucastnici/index");
+    modelAndView.addObject("ucastnici",seznamUcastniku);
+    return modelAndView;
+  }
+
+  @GetMapping("/detail/{id}")
+  public ModelAndView detail(@PathVariable int id) {
+    ModelAndView modelAndView = new ModelAndView("/lektori/detail");
+    modelAndView.addObject("lektor", seznamUcastniku.get(id));
     return modelAndView;
   }
 
